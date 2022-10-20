7e591a177269c231083ea1d85b35489ebcfcd54e
Amir-Adel9 <amiradel7152@gmail.com>
2022-10-20 13:59:14 +0200

    Added caching

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,19 +1,30 @@
 import express, { Request, Response } from 'express';
 import resize from '../../imageActions';
 import imagesDir from '../../globals';
+import fs from 'fs';
 
 const image = express.Router();
 
 image.get('/', async (req: Request, res: Response): Promise<void> => {
   const { filename, width, height } = req.query;
-  if (filename && width && height) {
-    await resize(String(filename), Number(width), Number(height)).catch(
-      (error) => res.status(404).send(error.message)
-    );
+
+  const cacheFile = `${imagesDir}/edited/${
+    filename + ' w-' + width + ' h-' + height
+  }.jpg`;
+  if (fs.existsSync(cacheFile)) {
     res.sendFile(
       `${imagesDir}/edited/${filename + ' w-' + width + ' h-' + height}.jpg`
     );
-  } else res.sendFile(imagesDir + '/fjords.jpg');
+  } else {
+    if (filename && width && height) {
+      await resize(String(filename), Number(width), Number(height)).catch(
+        (error) => res.status(404).send(error.message)
+      );
+      res.sendFile(
+        `${imagesDir}/edited/${filename + ' w-' + width + ' h-' + height}.jpg`
+      );
+    } else res.sendFile(imagesDir + '/fjords.jpg');
+  }
 });
 
 export default image;
